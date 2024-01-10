989e71aa8869247e492e12703de4aad12a385249
yosri-boujnah <yosriboujnah84@gmail.com>
2024-01-10 08:48:06 +0100

    'myfirstproj'

--- JQ-work.js
+++ JQ-work.js
@@ -1,9 +1,7 @@
 
-
-
-
-function makeprudect(name,img,price,ingredient,category){
+function makeprudect(id,name,img,price,ingredient,category){
     return{
+        id:0,
         name:name,
         img:img,
         price:price,
@@ -12,26 +10,25 @@ function makeprudect(name,img,price,ingredient,category){
     }
 }
 //ICE COFFE
-var icevanill=makeprudect(vanillaIceCoffe,'img/download.jpg',8,cofee-ice-cubes-vanilla-serum-CremFraiche,iceCoffe)
-var iceChokola=makeprudect(ChokolaIceCofee,'img/Iced-Mocha.jpg',9,cofee-ice-cubes-chokola-serum-CremFraiche,iceCoffe)
-var iceBanan=makeprudect(IceCoffeBanan,'img/coconut_banana_iced_latte.png',10,cofee-ice-cubes-banan-serum-CremFraiche,iceCoffe)
-var Icefrapp=makeprudect(Frappuccino,'img/double-chocolate-chip-frap.jpg',11,ice-espresso-milk_chocolatesyrup,iceCoffe)
+var icevanill=makeprudect(1,'vanilla_Ice_Coffe','iccecofee.png','8dt','the ingredient is: coffe ice cubes vanilla serum CremFraiche','iceCoffe')
+var iceChokola=makeprudect(2,'Chokola_Ice_Cofee','icecoff.png','9dt','the ingredient is:coffe ice cubes chokola serum CremFraiche','iceCoffe')
+var iceBanan=makeprudect(3,'Ice_Coffe_Banan','banan.png','10dt','the ingredient is:cofee ice cubes banan serum CremFraiche','iceCoffe')
+var Icefrapp=makeprudect(4,'Frappuccino','frappo.png','11dt','the ingredient is: ice espresso milk chocolatesyrup','iceCoffe') 
 //HOTCOFFE
-var cappucino=makeprudect(cappucino,'img/Capuccino.webp',4,'',hotCoffe)
-var espresso=makeprudect(Espresso,'img/Espresso-Taste-Metallic.webp',3,'',hotCoffe)
-var americano=makeprudect(americano,'img/americano-kahve-nedir-nasil-yapilir-9-faydasi.jpg',3,'',hotCoffe)
-var coffeCrem=makeprudect(coffeCrem,'img/cafe-creme-fritz-bertschi.webp',4,'',hotCoffe)
+var cappucino=makeprudect(5,'cappucino','cappoucino.png','4dt','','hotCoffe')
+var espresso=makeprudect(6,'Espresso','espresso.png','3dt','','hotCoffe')
+var americano=makeprudect(7,'americano','americano.jpg','3dt','','hotCoffe')
+var coffeCrem=makeprudect(8,'coffeCrem','caffecrem.png','4dt','','hotCoffe')
 //BREAKFAST
-var lumberjackt=makeprudect(lumberjacktBreakfast,'breakfast/download.jpg',15,sausage-eggs-tost-bacon-mayo,breakfast)
-var EggTartine=makeprudect(EggTartine,'breakfast/images.jpg',11,egg-tartine-tomate-avoca-mayo,breakfast)
-var japanesecrepe=makeprudect(JapaneseCrepe,'breakfast/crepe.jpg',14,chocla-banan-strawberry,breakfast)
-var omlette=makeprudect(Omlette,'breakfast/Basic-omelette-1024x656.jpg',14,eggs-cheese-vegtable,breakfast)
+var lumberjackt=makeprudect(9,'lumberjackt_Breakfast','download.jpg','15dt','the ingredient is:sausage eggs tost bacon mayo','breakfast')
+var EggTartine=makeprudect(10,'Egg_Tartine','Eggtart.png','11dt','the ingredient is:egg tartin avoca','breakfast')
+var japanesecrepe=makeprudect(11,'Japanese_Crepe','crep.png','14dt','the ingredient is:chocla banan strawberry','breakfast')
+var omlette=makeprudect(12,'Omlette','omlett.png','14dt','the ingredient is:eggs cheese vegtable','breakfast')
 //COOKISE
-var mango=makeprudect(mangoCashew,'cookise/mango.jpg',5,mango,Cookise)
-var bluberry=makeprudect(Bluberry-Cookise,'cookise/blueberry.jpg',5,bluberry,Cookise)
-var chocolate=makeprudect(chocolate-cookise,'cookise/chocclate chip.jpg',5,chocola,Cookise)
-var cranberry=makeprudect(Cranberry-pecan,'cookise/cranberry.jpg',5,cranberry,Cookise)
-var goldenMac=makeprudect(Golden_macadamia,'cookise/golden.jpg',5,macadamia,Cookise)
+var bluberry=makeprudect(14,'Bluberry-Cookise','blueberry.png','5dt','the ingredient is:bluberry','Cookise')
+var chocolate=makeprudect(15,'chocolate-cookise','choclat.png','5dt','the ingredient is:chocola','Cookise')
+var cranberry=makeprudect(16,'Cranberry-pecan','cammill.png','5dt','the ingredient is:cranberry','Cookise')
+var goldenMac=makeprudect(17,'Golden_macadamia','wayhek.png','5dt','the ingredient is:macadamia','Cookise')
 //
 var All=[
     icevanill,
@@ -46,74 +43,12 @@ var All=[
     EggTartine,
     japanesecrepe,
     omlette,
-    mango,
     bluberry,
     chocolate,
-    goldenMac
+    goldenMac,
+    cranberry
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function each(array, func) {
-    for (var i = 0; i < array.length; i++) {
-      func(array[i], i)
-    }
-  }
-  
-  function map(array, f) {
-    var acc = []
-    each(array, function (element, i) {
-      acc.push(f(element, i))
-    })
-    return acc
-  }
-  
-  function filter(array, predicate) {
-    var acc = []
-    each(array, function (element, index) {
-      if (predicate(element, index)) {
-        acc.push(element)
-      }
-    })
-    return acc
-  }
-  
-  function reduce(array, f, acc) {
-    if (acc === undefined) {
-      acc = array[0]
-      array = array.slice(1)
-    }
-    each(array, function (element, i) {
-      acc = f(acc, element, i)
-    })
-    return acc
-  }
-  //
-  var each = function (coll, func) {
+var each = function (coll, func) {
     if (Array.isArray(coll)) {
         for (var i = 0; i < coll.length; i++) {
             func(coll[i], i)
@@ -134,4 +69,66 @@ var filter = function (array, predicate) {
         }
     })
     return acc;
+}
+var filterByCategory=function (params) {
+    return filter(All,function (element) {
+        return element.category===params
+    })
+    
+}
+
+function show(All) {
+    each(All,function (element) {
+              $('.section-center').append(`<div class="item">
+              
+             <div> <img src=${element.img}></div>
+             <div> <h2>${element.name}</h2> &nbsp;
+              <h2>${element.price}</h2>&nbsp;
+              <h2>${element.ingredient}</h2></div>`)
+           })
+}
+// show (All)
+
+var test 
+$("#All").click(function () {
+    $('.section-center').empty()
+show(All)
+})
+$("#ice").on("click",function () {
+    $('.section-center').empty()
+     test = filterByCategory("iceCoffe")
+    show(test)
+})
+$("#hot").click(function () {
+    $('.section-center').empty()
+     test = filterByCategory("hotCoffe")
+    show(test)
+})
+$("#break").click(function () {
+    $('.section-center').empty()
+     test = filterByCategory("breakfast")
+    show(test)
+})
+$("#cook").on("click",function () {
+    $('.section-center').empty()
+     test = filterByCategory("Cookise")
+    show(test)
+})
+
+
+function filter(array, predicate) {
+    var acc = [];
+    each(array, function (element, index) {
+
+        if (predicate(element, index)) {
+
+            acc.push(element);
+        }
+    });
+    return acc;
+}
+function filterByCategory(query) {
+    return filter(products, function (ele, i) {
+        return ele.Categorie === query;
+    });
 }
