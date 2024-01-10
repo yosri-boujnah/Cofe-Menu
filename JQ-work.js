d6b43eebfc0451699ff1424dea439e3c1702a394
yosri-boujnah <yosriboujnah84@gmail.com>
2024-01-10 10:20:35 +0100

    'myfirstproj'

--- JQ-work.js
+++ JQ-work.js
@@ -10,25 +10,25 @@ function makeprudect(id,name,img,price,ingredient,category){
     }
 }
 //ICE COFFE
-var icevanill=makeprudect(1,'vanilla_Ice_Coffe','iccecofee.png','8dt','the ingredient is: coffe ice cubes vanilla serum CremFraiche','iceCoffe')
-var iceChokola=makeprudect(2,'Chokola_Ice_Cofee','icecoff.png','9dt','the ingredient is:coffe ice cubes chokola serum CremFraiche','iceCoffe')
-var iceBanan=makeprudect(3,'Ice_Coffe_Banan','banan.png','10dt','the ingredient is:cofee ice cubes banan serum CremFraiche','iceCoffe')
-var Icefrapp=makeprudect(4,'Frappuccino','frappo.png','11dt','the ingredient is: ice espresso milk chocolatesyrup','iceCoffe') 
+var icevanill=makeprudect(1,'name:vanilla_Ice_Coffe','iccecofee.png','price:8dt','the ingredient is: coffe ice cubes vanilla serum CremFraiche','iceCoffe')
+var iceChokola=makeprudect(2,'name:Chokola_Ice_Cofee','icecoff.png','price:9dt','the ingredient is:coffe ice cubes chokola serum CremFraiche','iceCoffe')
+var iceBanan=makeprudect(3,'name:Ice_Coffe_Banan','banan.png','price:10dt','the ingredient is:cofee ice cubes banan serum CremFraiche','iceCoffe')
+var Icefrapp=makeprudect(4,'name:Frappuccino','frappo.png','price:11dt','the ingredient is: ice espresso milk chocolatesyrup','iceCoffe') 
 //HOTCOFFE
-var cappucino=makeprudect(5,'cappucino','cappoucino.png','4dt','','hotCoffe')
-var espresso=makeprudect(6,'Espresso','espresso.png','3dt','','hotCoffe')
-var americano=makeprudect(7,'americano','americano.jpg','3dt','','hotCoffe')
-var coffeCrem=makeprudect(8,'coffeCrem','caffecrem.png','4dt','','hotCoffe')
+var cappucino=makeprudect(5,'name:cappucino','cappoucino.png','price:4dt','','hotCoffe')
+var espresso=makeprudect(6,'name:Espresso','espresso.png','price:3dt','','hotCoffe')
+var americano=makeprudect(7,'name:americano','americano.jpg','price:3dt','','hotCoffe')
+var coffeCrem=makeprudect(8,'name:coffeCrem','caffecrem.png','price:4dt','','hotCoffe')
 //BREAKFAST
-var lumberjackt=makeprudect(9,'lumberjackt_Breakfast','download.jpg','15dt','the ingredient is:sausage eggs tost bacon mayo','breakfast')
-var EggTartine=makeprudect(10,'Egg_Tartine','Eggtart.png','11dt','the ingredient is:egg tartin avoca','breakfast')
-var japanesecrepe=makeprudect(11,'Japanese_Crepe','crep.png','14dt','the ingredient is:chocla banan strawberry','breakfast')
-var omlette=makeprudect(12,'Omlette','omlett.png','14dt','the ingredient is:eggs cheese vegtable','breakfast')
+var lumberjackt=makeprudect(9,'name:lumberjackt_Breakfast','download.jpg','price:15dt','the ingredient is:sausage eggs tost bacon mayo','breakfast')
+var EggTartine=makeprudect(10,'name:Egg_Tartine','Eggtart.png','price:11dt','the ingredient is:egg tartin avoca','breakfast')
+var japanesecrepe=makeprudect(11,'name:Japanese_Crepe','crep.png','price:14dt','the ingredient is:chocla banan strawberry','breakfast')
+var omlette=makeprudect(12,'name:Omlette','omlett.png','price:14dt','the ingredient is:eggs cheese vegtable','breakfast')
 //COOKISE
-var bluberry=makeprudect(14,'Bluberry-Cookise','blueberry.png','5dt','the ingredient is:bluberry','Cookise')
-var chocolate=makeprudect(15,'chocolate-cookise','choclat.png','5dt','the ingredient is:chocola','Cookise')
-var cranberry=makeprudect(16,'Cranberry-pecan','cammill.png','5dt','the ingredient is:cranberry','Cookise')
-var goldenMac=makeprudect(17,'Golden_macadamia','wayhek.png','5dt','the ingredient is:macadamia','Cookise')
+var bluberry=makeprudect(14,'name:Bluberry-Cookise','blueberry.png','price:5dt','the ingredient is:bluberry','Cookise')
+var chocolate=makeprudect(15,'name:chocolate-cookise','choclat.png','price:5dt','the ingredient is:chocola','Cookise')
+var cranberry=makeprudect(16,'name:Cranberry-pecan','cammill.png','price:5dt','the ingredient is:cranberry','Cookise')
+var goldenMac=makeprudect(17,'name:Golden_macadamia','wayhek.png','price:5dt','the ingredient is:macadamia','Cookise')
 //
 var All=[
     icevanill,
